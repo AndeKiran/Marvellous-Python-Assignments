7b540f578c58d50d5ab92f77dafd65e65c4869e0
Kiran Ande <40136267+AndeKiran@users.noreply.github.com>
2025-05-20 00:26:09 +0530

    Update Assignment2_5.py

--- Assignment_2/Assignment2_5.py
+++ Assignment_2/Assignment2_5.py
@@ -1,11 +1,17 @@
 def main():
     print("Enter number to check if its prime number or not")
     value = int(input())
-    for a in range(1,value+1):
-        if(value%a == 0 and (a!=1 or a!=value)):
-            print("Number is prime")
-        else:
-            print("Number is not prime")
+    if(value == 1):
+        print("A prime number is a natural number greater than 1, Enter number greater than one")
+    else:    
+        for a in range(2,value+1):
+            if(value%a == 0):
+                if((a!=value)):
+                    print("Number is not prime")
+                    break
+                else:
+                    print("Number is prime")
+                    break
 
 if __name__ == "__main__":
     main()
