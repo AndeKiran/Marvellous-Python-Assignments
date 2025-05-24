d4bd11065eb55eadc08858bef15ed1075d2ac737
Kiran Ande <40136267+AndeKiran@users.noreply.github.com>
2025-05-24 15:42:52 +0530

    Delete Assignment_4/Assignment_5 directory

--- Assignment_4/Assignment_5/Assignment5_5.py
+++
@@ -1,9 +0,0 @@
-def main():
-    value = int(input(("Enter a number: ")))
-    if(value%2 == 0):
-        print(value,"is an even number")
-    else:
-        print(value,"is an odd number")
-
-if __name__ == "__main__":
-    main()
