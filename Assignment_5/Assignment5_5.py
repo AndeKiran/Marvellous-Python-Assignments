3477dfaabb400df6511d28d448c1169a5d1a23ff
Kiran Ande <40136267+AndeKiran@users.noreply.github.com>
2025-05-24 15:43:35 +0530

    Add files via upload

---
+++ Assignment_5/Assignment5_5.py
@@ -0,0 +1,9 @@
+def main():
+    value = int(input(("Enter a number: ")))
+    if(value%2 == 0):
+        print(value,"is an even number")
+    else:
+        print(value,"is an odd number")
+
+if __name__ == "__main__":
+    main()
